79f32b97f41ccf1e8287a2997d0266662d21bf96
Jonas malmevik <jonas.malmevik@gmail.com>
2023-07-22 18:27:42 +0200

    initial portals

--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelController : MonoBehaviour
+public class WheelController : PortalTraveller
 {
 
     [SerializeField] private WheelCollider frontRight;
